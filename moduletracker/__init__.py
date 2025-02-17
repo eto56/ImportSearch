b30ff9e442819d7a5b8d158b8ecfe9862eaa4fde
eto56 <arimakishou0505@gmail.com>
2025-02-17 21:31:26 +0900

    first release

--- moduletracker/__init__.py
+++
@@ -1,2 +0,0 @@
-from moduletracker.moduletracker import *
-
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from moduletracker import moduletracker
 
 setup(
-    name='moduletracker',
+    name='ImportSearch',
     version='0.1',
     packages=find_packages(),
     install_requires=[
